8433a349dcd737fc9f9d3d7db2a861c0fbfae221
Rosanna Zorzutti <94189034+Ross132020@users.noreply.github.com>
2022-01-27 16:39:34 +0000

    sommato valori file, tenendo conto solo dei valori e non delle date

--- shampoo.py
+++ shampoo.py
@@ -1,30 +1,56 @@
-my_file = open('shampoo_sales.csv', 'r')
-    print (my_file.read() [0:50]) #slicing
-    #slice dal carttere 0 incluso al 50esimo escluso
-
-        #r sta per read
-        #per scrivere si mette rw
-    contenuto= my_file.read()
+#print (my_file.read() [0:50]) #slicing
+#slice dal carttere 0 incluso al 50esimo escluso
 #stampo solo 50 cartteri per non riempire il foglio e avere un'idea di cosa ci sia dentro
 #stampo quindi i primi 50caratteri
-    if len(contenuto) > 50: 
-        print(contenuto[0:50] + '...') #2stringhe
-else: print(contenuto)
+#contenuto=my_file.read()
+    #if len(contenuto) > 50: 
+        #print(contenuto[0:50] + '...') #2stringhe
+#else: print(contenuto)
 
 # print(my_file.readline())   #spaziatura in aggiunta
 
-#faccio riga per riga
-      for line in my_file:
-   #Faccio lo split di ogni riga sulla virgola
-      elements = line.split(',')
-   #Faccio lo split di ogni riga sulla virgola
 
-# Se NON sto processando l’intestazione...
-if elements[0] != 'Date':
-# Setto la data e il valore
-        date = elements[0]
-        value = elements[1]
-# Aggiungo alla lista dei valori questo valore
-values.append(float(value))   
+# Inizializzo una lista vuota per salvare i valori 
+#values = []
+# Apro e leggo il file, linea per linea 
+#myfile=open('shampoo_sales.csv','r') 
+#print(myfile.readline())
+   #r sta per read
+   #per scrivere si mette rw
+#for line in myfile:
+    #elements=line.split(',')
+# Faccio lo split di ogni riga sulla virgola 
+    
+# Se NON sto processando l’intestazione... 
+    #if elements[0] != 'Date': # Setto la data e il valore 
+        #date = elements[0] 
+        #value = elements[1]
+        # Aggiungo alla lista dei valori questo valore 
+        #values.append(value)
 
-my_file.close()
+#print('lista:{}'.format(myfile))
+
+
+
+
+
+def somma_valori(myfile):
+    values=[]
+    for line in myfile:
+        elements=line.split(',')
+        if (elements[0] != 'Date'):
+            numero=elements[1]
+            values.append(float(numero))
+    myfile.close()
+    return(sum(values))
+myfileshampoo=open('shampoo_sales.csv','r')
+risultato=somma_valori(myfileshampoo)
+print('risultato somma: {}'.format(risultato))
+
+
+file2=open('shampoo_sales.csv','r')
+print(file2.readline())
+print(file2.readline())
+for line in file2:
+    elementi=(line.split(','))
+    print(elementi)
